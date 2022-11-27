91138b684adb1321182833c1bfd60f15745e39c7
TaylanPeixoto <taylanpeixotodasilva@gmail.com>
2022-11-27 15:28:59 -0300

    Mudança de nome do app, icone, tela de carregamento

---
+++ QRCode++/app/src/main/java/net/smallacademy/qrapp/SplashScreen.java
@@ -0,0 +1,30 @@
+package net.smallacademy.qrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(it);
+                finish();
+            }
+        }, 3000);
+    }
+
+}
