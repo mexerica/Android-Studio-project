c8b18acff0947dbfe0704017354553c8ff53e288
mexerica <produtivo63@gmail.com>
2022-11-26 21:53:19 -0300

    versão melhor do QR code

---
+++ QRCode++/QR-APP-master/app/src/main/java/net/smallacademy/qrapp/MainActivity.java
@@ -0,0 +1,137 @@
+package net.smallacademy.qrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import java.io.File;
+import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Date;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    EditText qrvalue;
+    Button generateBtn,scanBtn, choiceBtn, zapBtn;
+    ImageView qrImage;
+    String barcode = "QR Code";
+    File imgQR = null;
+    Bitmap mBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        qrvalue = findViewById(R.id.qrInput);
+        generateBtn = findViewById(R.id.generateBtn);
+        scanBtn = findViewById(R.id.scanBtn);
+        choiceBtn = findViewById(R.id.choiceBtn);
+        qrImage = findViewById(R.id.qrPlaceHolder);
+        zapBtn = findViewById(R.id.zapBtn);
+
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = qrvalue.getText().toString().trim();//getting text from input text field.
+                if(data.isEmpty() || barcode.equals("Barcode") && !data.matches("[0-9]+")){
+                    qrvalue.setError("Value Required.");//writing a warning
+                }else {
+                    if (barcode.equals("QR Code") && data.contains("Wiki")) { //link to wikipedia
+                        data = data.replace("Wiki ", "");
+                        data = "https://pt.wikipedia.org/wiki/" + data;
+                    }
+                    MultiFormatWriter mWriter = new MultiFormatWriter();
+                    try {
+                        BarcodeEncoder mEncoder = new BarcodeEncoder();
+                        BitMatrix mMatrix = mWriter.encode(data, escolher(), 400,400);
+                        mBitmap = mEncoder.createBitmap(mMatrix); //creating bitmap of code
+                        qrImage.setImageBitmap(mBitmap); //Setting generated QR code to imageView
+                        zapBtn.setEnabled(true);
+                        zapBtn.setBackgroundColor(Color.GREEN);
+                    }
+                    catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        choiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (barcode.equals("QR Code")) barcode = "Barcode";
+                else if (barcode.equals("Barcode")) barcode = "Data Matrix";
+                else if (barcode.equals("Data Matrix")) barcode = "PDF 417";
+                else if (barcode.equals("PDF 417")) barcode = "Aztec";
+                else barcode = "QR Code";
+                choiceBtn.setText(barcode);
+            }
+        });
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Scanner.class));
+            }
+        });
+
+        zapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent();
+                imgQR = createFile();
+                OutputStream outStream = null;
+                try {
+                    String pathofBmp= MediaStore.Images.Media.insertImage(getContentResolver(), mBitmap,"title", null);
+                    Uri uri = Uri.parse(pathofBmp);
+                    Intent.setAction(Intent.ACTION_SEND);
+                    Intent.putExtra(Intent.EXTRA_STREAM, uri);
+                    Intent.setType("image/*");
+                    Intent shareIntent = Intent.createChooser(Intent, null);
+                    startActivity(shareIntent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public BarcodeFormat escolher(){
+        switch (barcode) {
+            case "QR Code" : return BarcodeFormat.QR_CODE;
+            case "Barcode" : return BarcodeFormat.CODABAR;
+            case "Data Matrix" : return BarcodeFormat.DATA_MATRIX;
+            case "PDF 417" : return BarcodeFormat.PDF_417;
+            default : return BarcodeFormat.AZTEC;
+        }
+    }
+
+    private File createFile(){
+        File checKFile = new File(Environment.getExternalStorageDirectory()+"/imgQR/");
+        if(checKFile.exists()) checKFile.mkdirs();
+        Date currentTime = Calendar.getInstance().getTime();
+        File file = new File(Environment.getExternalStorageDirectory() + "/imgQR/"+"QR-"+currentTime+".jpg");
+        return file;
+    }
+
+    private void sendFile(){
+        Uri imageUri = Uri.parse(imgQR.getAbsolutePath());
+        zapBtn.setBackgroundColor(Color.RED);
+    }
+}
